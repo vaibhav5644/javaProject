b111412aa3dcb537c65bccc00e074d99ba99f779
vaibhav5644 <vaibhavgaikwad250@gmail.com>
2022-12-24 17:40:19 +0530

    Second commit : added LinkedList

---
+++ src/com/vaibhav/Example.java
@@ -0,0 +1,21 @@
+package com.vaibhav;
+
+import java.util.LinkedList;
+
+public class Example {
+
+	
+	public static void main(String[] args) {
+		
+		
+		LinkedList<Integer> list = new LinkedList<>();
+		
+		list.add(10);
+		list.add(20);
+		list.add(40);
+		list.add(50);
+		list.add(60);
+		
+		System.out.println(list);
+	}
+}
